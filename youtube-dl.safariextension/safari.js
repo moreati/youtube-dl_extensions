ef31c8bb3bd162b7b9378059c9c920bf305f7c6a
Jaime Marquínez Ferrándiz <jaimemf93@gmail.com>
2013-03-21 14:20:41 +0100

    Adapt the Safari extension to the youtube-dl-web javascript code

--- youtube-dl.safariextension/safari.js
+++ youtube-dl.safariextension/safari.js
@@ -1,26 +1,28 @@
-function download_link(url) {
-	return url;
-}
-
-function download_video(url) {
-	safari.application.activeBrowserWindow.openTab().url = url
-}
-
-function set_download_link(a, url) {
-	a.href=url
-	a.addEventListener("click",function() {download_video(url);}, false)
-}
+var popover_document;
 
 function load_videos(event) {
 	var target = event.target;
 	target.showPopover();
+	popover_document = target.popover.contentWindow.document;
 	var url = safari.application.activeBrowserWindow.activeTab.url;
-	set_loading_view();
 	processVideosURL(url);
 }
 
-function use_localserver() {
-	return safari.extension.settings.localserver;
+InfoViewer.prototype.html_video_link_on_click = function(url) {
+	return function() {safari.application.activeBrowserWindow.openTab().url = url;};
 }
+InfoViewer.prototype.video_section = function() {
+	return popover_document.getElementById('videos-section');
+}
+
+function localserverChanged(event) 
+{
+	if(event.key == "localserver") {
+		console.log("localserver settings has changed");
+		localStorage.localserver = safari.extension.settings.localserver;
+	}
+}
+
+safari.extension.settings.addEventListener("change",localserverChanged,false);
 
 safari.application.addEventListener("command", load_videos, false);
